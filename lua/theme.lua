943d001cbead71e55668c65e3df9d150b79e3716
Ian <iantirso.cini@gmail.com>
2024-08-25 09:14:52 +0200

    chore: remove duplicated line

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,7 +1,8 @@
---vim.opt.termguicolors = true
 --vim.opt.t_Co = 256
 vim.opt.background = "dark"
 vim.opt.termguicolors = true
+
+-- Monokai scheme
 vim.g['sonokai_disable_terminal_colors'] = 1
 vim.g['sonokai_better_performance'] = 1
 vim.g['sonokai_style'] = "atlantis"
